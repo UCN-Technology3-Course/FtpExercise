eebb3c4e12dc05de10504e6353fb9f9b15d587b2
Lars Nysom <LANY@ucn.dk>
2020-10-04 10:09:06 +0200

    Add project files.

---
+++ FtpExercise/FtpClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FtpExercise
+{
+    class FtpClient
+    {
+        public IEnumerable<string> GetFileList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UploadFile(string localpath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DownloadFile(string remotepath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
