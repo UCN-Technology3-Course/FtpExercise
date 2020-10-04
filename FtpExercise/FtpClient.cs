708d328b7ccee7798a01e71dbc90276cc318b080
Lars Nysom <LANY@ucn.dk>
2020-10-04 10:15:15 +0200

    Added TODO descriptions

--- FtpExercise/FtpClient.cs
+++ FtpExercise/FtpClient.cs
@@ -10,16 +10,22 @@ namespace FtpExercise
     {
         public IEnumerable<string> GetFileList()
         {
+            // TODO: Get list of files from the ftp server and return a list of filenames.
+
             throw new NotImplementedException();
         }
 
         public void UploadFile(string localpath)
         {
+            // TODO: Upload the file defined in the localpath parameter to the ftp server.
+
             throw new NotImplementedException();
         }
 
-        public void DownloadFile(string remotepath)
+        public void DownloadFile(string remotefilename)
         {
+            // TODO: Download the file defined in the remotefilename from the ftp server and write it to your harddrive.
+
             throw new NotImplementedException();
         }
     }
